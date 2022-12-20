76469e431b22d215bacc5644026775e84ed9f02e
cheesestraws <rob@midworld.co.uk>
2022-12-20 17:52:44 +0000

    remove old Mac MiniVNC source

--- mac-cpp-source/auxutils.h
+++
@@ -1,6 +0,0 @@
-#ifndef AUXUTILS_H
-#define AUXUTILS_H
-
-void auxinit(int alertID);
-
-#endif
--- mac-cpp-source/libs/Ari Halberstadt/OSUtilities.h
+++
@@ -1,6 +0,0 @@
-/* (c) Copyright 1994 Ari Halberstadt */
-
-typedef long ThreadTicksType;                   /* clock ticks */
-
-Boolean EventGet(short mask, EventRecord *event, ThreadTicksType sleep, RgnHandle cursor);
-void SetDText(DialogPtr dlg, short item, const Str255 str);
